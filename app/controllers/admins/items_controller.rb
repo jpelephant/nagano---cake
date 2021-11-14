f5749553f6ddf3d8501a4da5a0465e9654fe2b60
EC2 Default User <ec2-user@ip-172-31-9-51.ec2.internal>
2021-11-14 15:03:19 +0000

    [Add]機能

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,11 +1,18 @@
 class Admins::ItemsController < ApplicationController
   def index
+    @items = Item.all
+    @item = Item.all.page(params[:page]).per(10)
   end
   
   def new
+    @item =Item.new
   end
   
   def create
+    @item = Item.new(item_params)
+    @item.user_id = current_user.id
+    @item.save
+    redirect_to items_path
   end 
   
   def show
@@ -16,4 +23,11 @@ class Admins::ItemsController < ApplicationController
   
   def update
   end
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image_id, :genre_id, :introduction, :price)
+  end
 end
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  
+  def add_tax_price
+        (self.price * 1.08).round
+  end
 end
