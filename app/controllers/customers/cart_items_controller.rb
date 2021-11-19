426fa39fb9e20b7b8999ac8bf3cffcd9dc624873
EC2 Default User <ec2-user@ip-172-31-9-51.ec2.internal>
2021-11-19 09:39:15 +0000

    [Add]customerファイル

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -1,14 +1,20 @@
 class Customers::CartItemsController < ApplicationController
   def update
+    @cart_items = Cart_item.all
   end
   
   def update
   end
   
   def destroy
+    @cart_item = Cart_item.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_item_path
   end
   
   def destroy_all
+    @cart_item = Cart_item.destroy
+    redirect_to cart_item_path
   end
   
   def create
--- app/controllers/customers/items_controller.rb
+++ app/controllers/customers/items_controller.rb
@@ -1,7 +1,16 @@
 class Customers::ItemsController < ApplicationController
   def index
+    @items = Item.all.page(params[:page])
+    @item = Item.all
+    @genres = Genre.all
   end
   
   def show
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+    private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
   end
 end
